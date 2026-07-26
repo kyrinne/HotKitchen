b79e8551f37d0964aa2925419621da5f0e20060e
kyrinne <noreply@cyrinne.dev>
2026-07-26 09:09:36 +0200

    Initial code provided by Hyperskill. Excludes course files.

---
+++ HotKitchen/task/src/Application.kt
@@ -0,0 +1,12 @@
+package hotkitchen
+
+import hotkitchen.plugins.configureRouting
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
