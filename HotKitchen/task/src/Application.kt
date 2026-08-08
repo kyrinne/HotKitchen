9d333fd997eed310a3d9e0dd7c250ea18afb51ad
kyrinne <noreply@cyrinne.dev>
2026-08-08 08:37:45 +0200

    WIP. Add basic input validation for signup. Add JWT token responses.

--- HotKitchen/task/src/Application.kt
+++ HotKitchen/task/src/Application.kt
@@ -1,17 +1,26 @@
 package hotkitchen
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import hotkitchen.db.UserDaoImpl
-import hotkitchen.db.UserTable
 import hotkitchen.plugins.configureRouting
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.auth.Authentication
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.respond
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.transactions.transaction
-import java.util.UUID
+
+// TODO: do this properly
+const val secret = "3v4IUndwTyDNVAwQcTyM9Ty/Z8qzpecilZTzaOevqlA="
+const val issuer = "http://0.0.0.0:28852/"
+const val audience = "http://0.0.0.0:28852"
+const val myRealm = "Access to hot kitchen"
+
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -25,6 +34,31 @@ fun Application.module() {
             prettyPrint = true
             isLenient = true
         })
+    }
+    install(Authentication) {
+        jwt {
+            realm = myRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build()
+            )
+            // TODO
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            // TODO
+            challenge { defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
+
+        }
     }
 
     val database = Database.connect(
--- HotKitchen/task/src/plugins/Routing.kt
+++ HotKitchen/task/src/plugins/Routing.kt
@@ -1,19 +1,52 @@
 package hotkitchen.plugins
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import hotkitchen.audience
 import hotkitchen.db.UserDaoImpl
+import hotkitchen.issuer
 import hotkitchen.model.User
+import hotkitchen.secret
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.Parameters
 import io.ktor.server.application.*
 import io.ktor.server.request.receive
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.util.Date
+import kotlin.collections.hashMapOf
 
-// TODO: clean up
+// TODO: clean up - figure out a good file structure
 @Serializable
 data class Login(val email: String, val password: String)
+
+@Serializable
+data class Token(val token: String)
+
+// TODO: username, email, ...?
+fun createJWT(username: String): String? = JWT.create()
+    .withAudience(audience)
+    .withIssuer(issuer)
+    .withClaim("username", username)
+    .withExpiresAt(Date(System.currentTimeMillis() + 24 * 60 * 60000))
+    .sign(Algorithm.HMAC256(secret))
+
+/**
+ * [email] is valid if there is a prefix and a domain
+ */
+fun validateEmail(email: String): Boolean = email.matches(Regex(".+@.+\\..+"))
+
+/**
+ * [password] is valid if it is at least 6 characters long and contains at least one letter and one number
+ */
+fun validatePassword(password: String): Boolean {
+    return password.length >= 6
+            && password.contains(Regex("[0-9]+"))
+            && password.contains(Regex("[a-zA-Z]+"))
+}
 
 fun Application.configureRouting(dao: UserDaoImpl) {
     routing {
@@ -28,11 +61,20 @@ fun Application.configureRouting(dao: UserDaoImpl) {
             try {
                 val user = call.receive<User>()
                 if (dao.allUsers().map { it.email }.contains(user.email)) {
-                    call.respond(HttpStatusCode.Forbidden, Status("Registration failed"))
+                    call.respond(HttpStatusCode.Forbidden, Status("User already exists"))
                 } else {
                     // TODO: clean up
-                    dao.addUser(user.email, user.userType, user.password)
-                    call.respond(Status("Signed Up"))
+                    if (!validateEmail(user.email)) {
+                        call.respond(HttpStatusCode.Forbidden, Status("Invalid email"))
+                    }
+                    if (!validatePassword(user.password)) {
+                        call.respond(HttpStatusCode.Forbidden, Status("Invalid password"))
+                    }
+                    if (validateEmail(user.email) && validatePassword(user.password)) {
+                        dao.addUser(email = user.email, userType = user.userType, password = user.password)
+                        val token = createJWT(user.email)!! // TODO
+                        call.respond(Token(token))
+                    }
                 }
             } catch (_: Throwable) {
                 call.respond(HttpStatusCode.Forbidden, Status("Registration failed"))
@@ -42,9 +84,9 @@ fun Application.configureRouting(dao: UserDaoImpl) {
         post("/signin") {
             try {
                 val (email, password) = call.receive<Login>()
-                println("$email $password")
                 if (dao.allUsers().singleOrNull() { it.email == email && it.password == password } != null) {
-                    call.respond(Status("Signed In"))
+                    val token = createJWT(email)!! // TODO
+                    call.respond(Token(token))
                 } else {
                     call.respond(HttpStatusCode.Forbidden, Status("Authorization failed"))
                 }
